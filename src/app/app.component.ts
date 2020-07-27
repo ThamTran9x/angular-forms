3f9d35abfb577cd4013bcb749e1282ea79e97e25
tham tran <thamtran9x@gmail.com>
2020-07-27 14:55:07 +0200

    patchValue in form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,19 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
-    this.signUpForm.setValue({
+    // this.signUpForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'female'
+    // });
+    this.signUpForm.form.patchValue({
       userData: {
-        username: suggestedName,
-        email: ''
-      },
-      secret: 'pet',
-      questionAnswer: '',
-      gender: 'female'
+        username: suggestedName
+      }
     });
   }
 
