7fda95ab692aa203ac3c8222a0a6b3841b985952
tham tran <thamtran9x@gmail.com>
2020-07-27 15:16:05 +0200

    using form data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,15 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -35,7 +44,11 @@ export class AppComponent {
   // }
 
   onSubmit(f: NgForm) {
-    console.log(f);
-
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
+    this.submitted = true;
   }
 }
